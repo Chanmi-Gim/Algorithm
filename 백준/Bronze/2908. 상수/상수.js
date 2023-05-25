8c94826a4a789088a81e40ffc577e5d6845c91fd
Chanmi-Gim <117872640+Chanmi-Gim@users.noreply.github.com>
2023-05-25 14:44:45 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.js
+++ 백준/Bronze/2908. 상수/상수.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : `${__dirname}/input.txt`;
 let input = fs.readFileSync(filePath).toString().trim().split(' ')
-let [a, b] = [input[0].split(''), input[1].split('')];
-console.log(Math.max(a.reverse().join(''), b.reverse().join('')))
+let a = input[0].split('').reduce((acc, el)=>el+ acc, "")
+let b = input[1].split('').reduce((acc, el)=>el+ acc, "")
+console.log(Math.max(a, b))
