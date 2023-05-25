c2241931a0258b423bfa285c0d8c524657209488
Chanmi-Gim <117872640+Chanmi-Gim@users.noreply.github.com>
2023-05-25 14:16:37 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 132 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : `${__dirname}/input.txt`;
+let input = fs.readFileSync(filePath).toString().trim()
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+console.log(alphabet.map((x)=> input.includes(x)? input.indexOf(x) : -1).join(' '))
