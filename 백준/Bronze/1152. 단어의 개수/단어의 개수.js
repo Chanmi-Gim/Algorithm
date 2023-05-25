f39f2188d30f4f35df54aaa69cc6b4958e11fda6
Chanmi-Gim <117872640+Chanmi-Gim@users.noreply.github.com>
2023-05-25 14:52:01 +0900

    [Bronze II] Title: 단어의 개수, Time: 148 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,3 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : `${__dirname}/input.txt`;
+let input = require("fs").readFileSync(filePath).toString().trim().split(' ')
+console.log(input[0] == ''? 0: input.length)
