13c3964a3b4fdb6b0f8adbf4069687286932dcd6
Chanmi-Gim <117872640+Chanmi-Gim@users.noreply.github.com>
2023-06-04 01:36:48 +0900

    [unrated] Title: 귤 고르기, Time: 44.73 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -1,12 +1,12 @@
 function solution(k, tangerine) {
     let hash = new Map();
-    let answer = [];
-    for(one of tangerine) hash.set(one, hash.get(one)+1 || 1);
+    let answer = 0;
+    for(one of tangerine) hash.set(one, (hash.get(one) || 0) + 1);
     let entries = Array.from(hash.entries()).sort((a,b)=>b[1]-a[1])
-    entries.reduce((acc, [_,num], i)=> {
-        acc += num
-        if(acc >= k) answer.push(i+1)
-        return acc
-    }, 0)
-    return answer[0]
+    for(const [_, num] of entries){
+        answer++; 
+        if(k > num) k -= num;
+        else break;
+    }
+    return answer
 }
