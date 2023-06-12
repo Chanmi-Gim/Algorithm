6d0bfa16ee9128343e05c083bd58b45480d56f6a
Chanmi-Gim <117872640+Chanmi-Gim@users.noreply.github.com>
2023-06-12 22:49:16 +0900

    [level 2] Title: 올바른 괄호, Time: 6.36 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,33 +1,12 @@
-// function solution(s) {
-//     let stack = [];
-//     let arr = s.split("");
-
-//     // "("가 없거나 첫 번째에 없는 경우
-//     let idx = arr.findIndex((x) => x === "(");
-//     if (idx === -1 || idx !== 0) return false;
-//     // "("가 있는 경우
-//     else {
-//         arr = arr.splice(idx, arr.length - idx);
-//         arr.map((el) => {
-//             if (el === "(") {
-//                 stack.push("(");
-//             } else {
-//                 stack.pop("(");
-//             }
-//         });
-//     }
-//     return stack.length === 0
-// }
-
-function solution(s) {
-    let stack =[]
-    let idx = s.indexOf("(")
-    if(idx == -1 || idx !== 0) return false
-    else{
-        s = s.slice(idx)
-        for (str of s){
-            str== "("? stack.push(1): stack.pop();
+function solution(s){  
+    let stack = [];
+    for(const x of s){
+        if(x === "(") stack.push("(")
+        else{
+            if(stack.length === 0) return false
+            stack.pop()
         }
-        return stack.length === 0
-    } 
+    }
+    if(stack.length !== 0) return false
+    return true
 }
