9bbac75429a539d301ce3aed77ea4eb39339f180
chanmi-gim <117872640+Chanmi-Gim@users.noreply.github.com>
2023-11-07 01:17:14 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.27 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,15 +1,4 @@
 function solution(new_id) {
-    let answer = new_id.toLowerCase()
-    answer = answer.match(/[a-z0-9\_\.\-]/g).join('')
-    answer = answer.replace(/\.{2,}/g, ".")
-    answer = answer.replace(/(^\.)|(\.$)/, "")
-    if(answer.length === 0) answer += "a"
-    if(answer.length >= 16) answer = answer.slice(0,15)
-    answer = answer.replace(/\.$/,"")
-    if(answer.length <= 2) {
-        while(answer.length < 3){
-            answer += answer[answer.length-1]
-        }
-    }
-    return answer
+    var answer = '';
+    return answer;
 }
