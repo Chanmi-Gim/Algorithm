b6f334f3987c50ff48f14540ae4791ed34bc9f74
chanmi-gim <117872640+Chanmi-Gim@users.noreply.github.com>
2024-02-13 13:54:35 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,10 +1,7 @@
 function solution(rank, attendance) {
     const studentList = [];
     const map = new Map(); 
-    for (let i = 0; i < rank.length; i++) {
-        if(attendance[i]) map.set(rank[i], i)
-        else map.set(rank[i], -1)
-    }
+    rank.forEach((x, i)=> attendance[i]? map.set(x, i) : map.set(x, -1))
     const arr = Array.from(map.entries()).sort((a, b)=> a[0] - b[0])
     for(const [k, v] of arr) if(v !== (-1) && studentList.length <= 3) studentList.push(v)
     return studentList[0] * 10000 + studentList[1] * 100 + studentList[2]
