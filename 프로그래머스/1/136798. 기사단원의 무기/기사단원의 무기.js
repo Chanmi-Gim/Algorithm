e6f1f8187166af26cf583258f988841ed94ef794
chanmi-gim <117872640+Chanmi-Gim@users.noreply.github.com>
2024-02-13 16:57:53 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.40 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,14 +1,16 @@
 // 1. number의 약수를 구한다. 
 // 2. limit에 해당하는 수가 없으면 전체 합 리턴, 있으면 모두 power로 바꾼뒤 전체 합 리턴
-function solution(number, limit, power) {
-    let answer = 1; 
-    for(let i = 2; i <= number; i++){
-        let num = 1;
-        for(let j = 1; j <= i/2; j++) {
-            if (i % j === 0) num++;
-        }
-        if(num > limit) num = power;
-        answer += num;
+const ys = (number, limit, power) => {
+    let num = 1;
+    for(let j = 1; j <= number/2 ; j++){
+        if(number % j === 0) num++;
     }
-    return answer
+    return (num <= limit) ? num : power   
+}
+function solution(number, limit, power) {
+    let answer = 1;
+    for(let i = 2; i <= number; i++){
+        answer += ys(i, limit, power);
+    }
+    return answer;
 }
