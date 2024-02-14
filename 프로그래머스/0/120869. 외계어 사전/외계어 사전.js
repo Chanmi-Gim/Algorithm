bdb36cd924f1fa9714f8f97973759d47743b62c8
chanmi-gim <117872640+Chanmi-Gim@users.noreply.github.com>
2024-02-14 15:41:55 +0900

    [level 0] Title: 외계어 사전, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -1,3 +1,3 @@
 // const solution = (spell, dic) => dic.filter(x=> spell.every(el=> x.includes(el)));
 
-const solution = (spell, dic) => dic.map((word) => spell.every((x)=> word.includes(x))).some((x)=> x === true) ? 1 : 2 ;
+const solution = (spell, dic) => dic.filter((word) => spell.every((x)=> word.includes(x))).length ? 1 : 2 ;
