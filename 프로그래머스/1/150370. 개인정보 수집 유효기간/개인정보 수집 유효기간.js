9a76d808aa67fb3792d82da68111f3f50f9cab9d
chanmi-gim <117872640+Chanmi-Gim@users.noreply.github.com>
2024-02-19 15:41:38 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.61 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,24 +1,14 @@
 const dateCirculator = (p, m) => {
     let [year, month, day] = p.split('.').map((x) => Number(x));
-    month += m;
-    day -= 1;
-    if (day === 0) {
-        day = 28;
-        month -= 1;
-    }
+    (month += m), (day -= 1);
+    if (day === 0) (day = 28), (month -= 1);
     if (month > 12) {
-        if (month % 12 === 0) {
-            year += month / 12 - 1;
-            month = 12;
-        } else {
-            let addYear = parseInt(month / 12);
-            year += addYear;
-            month = month % 12;
-        }
+        year += month % 12 === 0 ? month / 12 - 1 : parseInt(month / 12);
+        month = month % 12 === 0 ? 12 : month % 12;
     }
-    console.log('🔥', year, month, day);
     return `${year}.${String(month).padStart(2, '0')}.${String(day).padStart(2, '0')}`;
 };
+
 
 function solution(today, terms, privacies) {
     const answer = [];
