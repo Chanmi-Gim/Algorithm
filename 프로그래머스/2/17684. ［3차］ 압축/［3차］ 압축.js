566c667e71cb1387d2ab2e06dba6a78a671635fe
chanmi-gim <117872640+Chanmi-Gim@users.noreply.github.com>
2024-02-27 03:16:50 +0900

    [level 2] Title: [3차] 압축, Time: 0.71 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,21 +1,19 @@
 const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 function solution(msg) {
-    let idx = 0;
+    let idx = 0, max = 1;
     const result = [];
-    let max = 1;
     while (idx < msg.length) {
         for (let i = idx + max; i >= idx; i--) {
-            let word = msg.slice(idx, i + 1);
-            if (alphabet.includes(word)) {
-                let curr = word;
+            let curr = msg.slice(idx, i + 1);
+            if (alphabet.includes(curr)) {
                 let next = msg[i + 1];
-                result.push(alphabet.indexOf(word) + 1);
+                result.push(alphabet.indexOf(curr) + 1);
                 if (next !== undefined) {
                     alphabet.push(curr + next);
                     max = Math.max(max, (curr + next).length)
                 }
-                idx += word.length;
+                idx += curr.length;
                 break;
             }
         }
