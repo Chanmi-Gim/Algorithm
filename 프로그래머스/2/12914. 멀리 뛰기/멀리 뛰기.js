0cb616b12e17cd8e40e63c6e11e54452aeab6202
chanmi-gim <117872640+Chanmi-Gim@users.noreply.github.com>
2024-03-09 00:48:31 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,7 @@
+function solution(n) { 
+    if(n === 0) return 0
+    let dp = new Array(n+1);
+    [dp[1], dp[2]] = [1, 2] 
+    for(let i = 3; i <= n ; i++) dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+    return dp[n]
+}
