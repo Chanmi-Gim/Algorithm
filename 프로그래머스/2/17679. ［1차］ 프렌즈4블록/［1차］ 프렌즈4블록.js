f3e4404c44c2bfdaa3da57f5efc3c7ef3bc031d4
chanmi-gim <117872640+Chanmi-Gim@users.noreply.github.com>
2024-03-15 00:31:36 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 8.96 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -12,7 +12,7 @@ const checking = (r, c, board, m, n) => {
 const rePainting = (m, n, board, visited) => {
     for (let col = 0; col < n; col++) {
         for (let row = 1; row < m; row++) {
-            if (visited[row][col] === false) {
+            if (!visited[row][col]) {
                 board[row][col] = false;
                 for (let k = row - 1; k >= 0; k--) {
                     [board[k][col], board[k + 1][col]] = [board[k + 1][col], board[k][col]];
@@ -33,7 +33,7 @@ function solution(m, n, board) {
         const visited = Array.from({ length: m }, () => Array(n).fill(true));
         for (let row = 0; row < m; row++) {
             for (let col = 0; col < n; col++) {
-                if (!checking(row, col, board, m, n) && board[row][col] !== false) {
+                if (!checking(row, col, board, m, n) && board[row][col]) {
                     visited[row][col] = false;
                     for (let k = 0; k < 3; k++) {
                         visited[row + dx[k]][col + dy[k]] = false;
@@ -44,8 +44,8 @@ function solution(m, n, board) {
         visited.flat().forEach((x) => {
             if (x === false) cnt++;
         });
-        answer += cnt;
         if (cnt === 0) break;
+        answer += cnt;
         board = rePainting(m, n, board, visited);
     }
     return answer;
