84ef1dad62ad334705ed8b8a3c789f1aadf786ee
chanmi-gim <117872640+Chanmi-Gim@users.noreply.github.com>
2024-03-15 14:16:50 +0900

    [level 2] Title: 2 x n 타일링, Time: 6.07 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -1,9 +1,9 @@
 function solution(n){
     const arr = Array.from({length : n + 2}, () => 0);
-    arr[0] = 0
-    arr[1] = 1;
-    for(let i = 2 % 1000000007 ; i <= (n + 1) % 1000000007 ; i++) {
-        arr[i] += (arr[i-1] % 1000000007) + (arr[i-2] % 1000000007)
+    arr[1] = 1
+    arr[2] = 1
+    for(let i = 3 ; i <= n + 1; i++) {
+        arr[i] += (arr[i-1] + arr[i-2]) % 1000000007
     }
-    return arr[arr.length - 1] % 1000000007
+    return arr[arr.length - 1] 
 }
