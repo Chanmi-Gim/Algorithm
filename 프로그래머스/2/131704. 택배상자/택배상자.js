f4e6e2e7c154f77f5064e965e2410067735bd5f5
chanmi-gim <117872640+Chanmi-Gim@users.noreply.github.com>
2024-03-18 16:38:22 +0900

    [level 2] Title: 택배상자, Time: 159.27 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.js
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -1,22 +1,21 @@
 function solution(order) {
     order.reverse();
-    const arr = Array.from({ length: order.length }, (_, i) => i + 1).reverse();
+    const belt = Array.from({ length: order.length }, (_, i) => i + 1).reverse();
     const stack = [];
     let cnt = 0;
     while (true) {
-        if (arr.length === 0 && stack[stack.length - 1] !== order[order.length - 1]) break;
-        if (arr.length === 0 && order.length === 0) break;
-        if (order[order.length - 1] !== arr[arr.length - 1]) {
+        if (belt.length === 0 && stack[stack.length - 1] !== order[order.length - 1]) break;
+        if (belt.length === 0 && order.length === 0) break;
+        if (order[order.length - 1] !== belt[belt.length - 1]) {
             if (stack[stack.length - 1] === order[order.length - 1]) {
                 stack.pop();
                 order.pop();
                 cnt++;
             } else {
-                let shift = arr.pop();
-                stack.push(shift);
+                stack.push(belt.pop());
             }
         } else {
-            arr.pop();
+            belt.pop();
             order.pop();
             cnt++;
         }
