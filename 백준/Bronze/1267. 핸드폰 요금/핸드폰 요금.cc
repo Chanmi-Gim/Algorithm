149af7f054a957c07d9a11a2d7cc43ac0bc57a6d
chanmi-gim <117872640+Chanmi-Gim@users.noreply.github.com>
2024-04-23 21:33:19 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 using namespace std;
 
-int Y = 0, M = 0, callingTime[10000];
+int N = 0, Y = 0, M = 0;
 int main(){
-  int N;
   cin >> N;
+  vector<int> callingTime(N);
   for(int i = 0 ; i < N ; i++) cin >> callingTime[i];
   for(int i = 0 ; i < N ; i++){
     Y += (floor(callingTime[i] / 30.0)+1) * 10;
