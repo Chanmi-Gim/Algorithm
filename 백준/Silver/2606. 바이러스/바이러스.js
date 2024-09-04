aa98e4a365ed8e586a6fdde538197aa29d484a4d
chanmi-gim <117872640+Chanmi-Gim@users.noreply.github.com>
2024-09-04 21:28:46 +0900

    [Silver III] Title: 바이러스, Time: 96 ms, Memory: 9460 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,20 +1,20 @@
-const fs = require('fs');
-const [node, k, ...input] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const [n, _, ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const arr = input.map((x) => x.split(' ').map(Number));
-const graph = Array.from({ length: Number(node) + 1 }, () => []);
-const visited = Array.from({ length: Number(node) + 1 }, () => 0);
-arr.forEach((item) => {
-    let [from, to] = item;
-    graph[to].push(from);
-    graph[from].push(to);
-});
-const dfs = (s, v) => {
-    visited[s] = 1;
-    for (let i = 0; i < graph[s].length; i++) {
-        if (!visited[graph[s][i]]) {
-            dfs(graph[s][i], v + 1);
+const visited = Array.from({ length: +n + 1 }, () => false);
+const graph = Array.from({ length: +n + 1 }, () => []);
+for (const [start, end] of arr) {
+    graph[start].push(end);
+    graph[end].push(start);
+}
+
+const dfs = (v) => {
+    visited[v] = true;
+    for (const n of graph[v]) {
+        if (!visited[n]) {
+            dfs(n);
         }
     }
 };
-dfs(1, 0);
-console.log(visited.filter((x) => x == 1).length - 1);
+dfs(1);
+const answer = visited.reduce((acc, el) => (el ? acc + el : acc), 0) - 1;
+console.log(answer);
