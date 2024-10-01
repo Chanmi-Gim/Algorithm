27178df455b524a3c2a6b2e8f0abf62c1fd510e5
chanmi-gim <117872640+Chanmi-Gim@users.noreply.github.com>
2024-10-01 20:34:49 +0900

    [Silver III] Title: 주유소, Time: 280 ms, Memory: 45140 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,10 +1,10 @@
 const arr = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const [_, roads, countries] = arr.map((x) => x.split(' ').map(Number));
+const [_, roads, countries] = arr.map((x) => x.split(' ').map((x) => BigInt(x)));
 
 let minValue = Number.MAX_SAFE_INTEGER;
-let cost = 0;
+let cost = BigInt(0);
 countries.forEach((country, i) => {
     if (country < minValue) minValue = country;
     if (i !== countries.length - 1) cost += roads[i] * minValue;
 });
-console.log(cost);
+console.log(String(cost));
